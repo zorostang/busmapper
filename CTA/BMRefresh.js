7b2ab0a59ccd25424d9dedacdada2597762c7149
Gino Bernardi <ginobernardi@att.net>
2015-10-11 13:27:19 -0500

    credits

--- CTA/BMRefresh.js
+++ CTA/BMRefresh.js
@@ -1,25 +1,27 @@
-SyncedCron.add({
-  name: 'CTATOKEN1',
-  schedule: function(parser) {
-    // parser is a later.parse object
-    return parser.recur().every(1).minute().between(0,29);
-;
-  },
-  job: function() {
-    busesUpdate(Meteor.settings.private.CTATOKEN1)
-  }
-});
+if (Meteor.isServer) {
+  SyncedCron.add({
+    name: 'CTATOKEN1',
+    schedule: function(parser) {
+      // parser is a later.parse object
+      return parser.recur().every(1).minute().between(0,29);
+  ;
+    },
+    job: function() {
+      busesUpdate(Meteor.settings.private.CTATOKEN1)
+    }
+  });
 
-SyncedCron.add({
-  name: 'CTATOKEN2',
-  schedule: function(parser) {
-    // parser is a later.parse object
-    return parser.recur().every(1).minute().between(30,60);
-;
-  },
-  job: function() {
-    busesUpdate(Meteor.settings.private.CTATOKEN2)
-  }
-});
+  SyncedCron.add({
+    name: 'CTATOKEN2',
+    schedule: function(parser) {
+      // parser is a later.parse object
+      return parser.recur().every(1).minute().between(30,60);
+  ;
+    },
+    job: function() {
+      busesUpdate(Meteor.settings.private.CTATOKEN2)
+    }
+  });
 
-SyncedCron.start();
+  SyncedCron.start();
+}
