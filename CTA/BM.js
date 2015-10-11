90118e4dcf7a23ba145578b66c678d09169e07b4
Gino Bernardi <ginobernardi@att.net>
2015-10-11 04:03:15 -0500

    server stuff

--- CTA/BM.js
+++ CTA/BM.js
@@ -1,36 +1,51 @@
 
 if (Meteor.isServer) {
-  Meteor.startup(function(){
-    var BusesRoutes = new Mongo.Collection("busesroutes");
+  Meteor.startup(function() {
+    let BusesRoutes = new Mongo.Collection("busesroutes");
+    let Buses = new Mongo.Collection("buses");
+
     HTTP.call("GET", "http://www.ctabustracker.com/bustime/api/v1/getroutes?key=mfZVaeUXL5HctzzxpFGyd5FNX",
-      function (err, res) {
+      function(err, res) {
         if (!err) {
-          var result = xml2js.parseStringSync(res.content)['bustime-response'];
-          var availableRoutes=result.route.map(function(route){
-              return route.rt;
-            }).reduce(function(pre,cur){
-              return pre.concat(cur)
-            },[]);
-          BusesRoutes.insert({'routes':availableRoutes},function(err,id){
-            if(!err){
-              routes=BusesRoutes.find().fetch()[0].routes;
-              routes.reduce(function(pre,cur,idx){
-                if((idx+1)%11==0){
-                  HTTP.call("GET", "http://www.ctabustracker.com/bustime/api/v1/getvehicles?key=mfZVaeUXL5HctzzxpFGyd5FNX&rt=".concat(pre),function(err,res){
+          let result = xml2js.parseStringSync(res.content)['bustime-response'];
+          let availableRoutes = result.route.map(function(route) {
+            return route.rt;
+          }).reduce(function(pre, cur) {
+            return pre.concat(cur);
+          }, []);
+          BusesRoutes.insert({'routes': availableRoutes }, function(err, id) {
+            if (!err) {
+              routes = BusesRoutes.find().fetch()[0].routes;
+              routes.reduce(function(pre, cur, idx) {
+                if ((idx + 1) % 11 === 0) {
+                  HTTP.call("GET", "http://www.ctabustracker.com/bustime/api/v1/getvehicles?key=mfZVaeUXL5HctzzxpFGyd5FNX&rt=".concat(pre), function(err, res) {
                     if (!err) {
-                      var result = xml2js.parseStringSync(res.content)['bustime-response'];
-                        console.log(result.vehicle)
+                      let result = xml2js.parseStringSync(res.content)['bustime-response'];
+                        if (result.hasOwnProperty('vehicle')) {
+                          
+                          
+                          result.vehicle.map((vehicle) => { 
+                            let _id = vehicle.vid[0];
+                            console.log(_id);
+                            _.extend(vehicle, {
+                              _id: _id,
+                            });
+                            Buses.insert(vehicle);
+                          });
+
+                          //console.log(result.vehicle);
+                          // Buses.insert(result.vehicle);
+                        }
                     }
-                  })
-                  return pre=[];
+                  });
+                  return pre = [];
+                } else {
+                  return pre.concat(cur);
                 }
-                else{
-                    return pre.concat(cur);
-                }
-              },[])
+              }, []);
             }
           });
         }
-    });
-  })
+      });
+  });
 }
